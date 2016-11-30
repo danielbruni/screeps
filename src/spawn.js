cc899a4b5a1583065f0f3c7dc5c9a5ca9b33a850
Daniel-Bruni Ziermann <danielbruni@users.noreply.github.com>
2016-11-30 10:05:39 +0100

    Update spawn.js

--- src/spawn.js
+++ src/spawn.js
@@ -1,33 +1,53 @@
 var helper = require('helper');
 
-module.exports = {
+var spawn = {};
 
-    // Amount of different Creeps to build
-    config: {
-        harvester: 5,
-        upgrader: 2,
-        builder: 3,
-        repairer: 2
-    },
+spawn.config = {
+    harvester: 5,
+    upgrader: 2,
+    builder: 3,
+    repairer: 2
+};
 
-    // Creep Templates
-    templates: {
-        harvester: [WORK,WORK,WORK,CARRY,CARRY,MOVE],
-        upgrader: [WORK,CARRY,CARRY,MOVE,MOVE],
-        builder: [WORK,CARRY,MOVE],
-        repairer: [WORK,CARRY,MOVE]
-    },
+spawn.templates = {
+    harvester: [WORK,CARRY,MOVE],
+    //harvester: [WORK,WORK,WORK,CARRY,CARRY,MOVE],
+    upgrader: [WORK,CARRY,CARRY,MOVE,MOVE],
+    builder: [WORK,CARRY,MOVE],
+    repairer: [WORK,CARRY,MOVE]
+};
 
-    run: function(){
-        for (var prop in this.config) {
-            if(this.config.hasOwnProperty(prop)) {
-                if(helper.getCreepByRole(prop).length < this.config[prop]) {
-                    var newCreep = Game.spawns['Spawn1'].createCreep(this.templates[prop], undefined, { role: prop });
-                    console.log('Spawning new Builder: ' + newCreep);
-                } else {
-                    console.log("We have enough " + prop);
-                }
-            }
-        }
+spawn.makeCreep = function(spawn, role) {
+    var newCreep = spawn.createCreep(this.templates[role], undefined, { role: role });
+    console.log ('Spawning new ' + role + ' with the Name: ' + newCreep);
+};
+
+spawn.run = function() {
+    // Return if the SpawnPoint has not enough Energy for a simple harvester
+    if (Game.spawns.Spawn1.energy < helper.getCreepCost(this.templates['harvester'])) {
+        return;
+    }
+
+    if (helper.getCreepsByRole('harvester').length < this.config.harvester)
+    {
+        this.makeCreep(Game.spawns.Spawn1, 'harvester');
+    }
+    else if (helper.getCreepsByRole('upgrader').length < this.config.upgrader)
+    {
+        this.makeCreep(Game.spawns.Spawn1, 'upgrader');
+    }
+    else if (helper.getCreepsByRole('builder').length < this.config.builder)
+    {
+        this.makeCreep(Game.spawns.Spawn1, 'builder');
+    }
+    else if (helper.getCreepsByRole('repairer').length < this.config.repairer)
+    {
+        this.makeCreep(Game.spawns.Spawn1, 'repairer');
+    }
+    else
+    {
+        console.log('Max Config reached. Nothing to spawn');
     }
 };
+
+module.exports = spawn;

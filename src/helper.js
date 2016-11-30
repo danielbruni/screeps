fac7f46dee206901c3ce30ac53ce6543290191e4
Daniel-Bruni Ziermann <danielbruni@users.noreply.github.com>
2016-11-30 10:04:57 +0100

    Added Creep Cost Helper

--- src/helper.js
+++ src/helper.js
@@ -9,8 +9,26 @@ helper.garbageCollection = function () {
     }
 };
 
-helper.getCreepByRole = function (roleName) {
+helper.getCreepsByRole = function (roleName) {
     return _.filter(Game.creeps, (creep) => creep.memory.role == roleName);
+};
+
+helper.getCreepCost = function(body) {
+    var cost = 0;
+    var bodyCost = {
+        "move": 50,
+        "carry": 50,
+        "work": 100,
+        "heal": 250,
+        "tough": 10,
+        "attack": 80,
+        "ranged_attack": 150,
+        "claim": 600
+    };
+
+    _.forEach(body, function (part) { cost += bodyCost[part]; });
+
+    return cost;
 };
 
 module.exports = helper;
